3563180bcea68542a8029e945d7aaaf87b6d6bd1
Arifin <arifin.1@hotmail.com>
2017-12-29 20:52:10 +0700

    ver.1

---
+++ resources/views/others/chat.blade.php
@@ -0,0 +1,37 @@
+<div class="col-xl-6 col-lg-12">
+  <div class="card">
+    <div class="card-header">
+      <h4 class="card-title">Basic Pills</h4>
+    </div>
+    <div class="card-body">
+      <div class="card-block">
+        <p>Takes the basic nav pills from above and adds the <code>.nav-pills</code> class to generate a pilled interface. </p>
+        <ul class="nav nav-pills">
+          <li class="nav-item">
+            <a class="nav-link active" id="base-pill1" data-toggle="pill" href="#pill1" aria-expanded="true">Pill 1</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="base-pill2" data-toggle="pill" href="#pill2" aria-expanded="false">Pill 2</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="base-pill3" data-toggle="pill" href="#pill3" aria-expanded="false">Pill 3</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+        <div class="tab-content px-1 pt-1">
+          <div role="tabpanel" class="tab-pane active" id="pill1" aria-expanded="true" aria-labelledby="base-pill1">
+            <p>Oat cake marzipan cake lollipop caramels wafer pie jelly beans. Icing halvah chocolate cake carrot cake. Jelly beans carrot cake marshmallow gingerbread chocolate cake. Gummies cupcake croissant.</p>
+          </div>
+          <div class="tab-pane" id="pill2" aria-labelledby="base-pill2">
+            <p>Sugar plum tootsie roll biscuit caramels. Liquorice brownie pastry cotton candy oat cake fruitcake jelly chupa chups. Pudding caramels pastry powder cake soufflé wafer caramels. Jelly-o pie cupcake.</p>
+          </div>
+          <div class="tab-pane" id="pill3" aria-labelledby="base-pill3">
+            <p>Biscuit ice cream halvah candy canes bear claw ice cream cake chocolate bar donut. Toffee cotton candy liquorice. Oat cake lemon drops gingerbread dessert caramels. Sweet dessert jujubes powder sweet sesame snaps.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
